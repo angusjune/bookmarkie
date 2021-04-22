3b5d85eb03c26efe1147a6c91b498daa61492d3a
angusjune <angus.is.online@gmail.com>
2021-04-22 16:43:12 +0800

    Added more sizes of icons

--- src/background.js
+++ src/background.js
@@ -9,32 +9,26 @@ const setIcon = (isDark = false) => {
         if (props.iconStyleAuto) {
             if (isDark) {
                 // set light-colored icon in dark UI
-                chrome.browserAction.setIcon({ path: `/images/${props.iconType}-light-32.png`});
+                chrome.browserAction.setIcon({ path: {
+                    16: `/images/${props.iconType}-light-16.png`,
+                    24: `/images/${props.iconType}-light-24.png`,
+                    32: `/images/${props.iconType}-light-32.png`,
+                }});
             } else {
-                chrome.browserAction.setIcon({ path: `/images/${props.iconType}-dark-32.png`});
+                chrome.browserAction.setIcon({ path: {
+                    16: `/images/${props.iconType}-dark-16.png`,
+                    24: `/images/${props.iconType}-dark-24.png`,
+                    32: `/images/${props.iconType}-dark-32.png`
+                }});
             }
         } else {
-            chrome.browserAction.setIcon({ path: `/images/${props.iconType}-${props.iconStyle}-32.png`});
+            chrome.browserAction.setIcon({ path: `/images/${props.iconType}-${props.iconStyle}-16.png`});
         }
     });   
 };
-
-// OS in dark mode or browser in incognito context
-const colorSchemeMatchMedia = window.matchMedia("(prefers-color-scheme: dark)");
-const isBrowserDark = colorSchemeMatchMedia.matches || chrome.extension.inIncognitoContext;
-setIcon(isBrowserDark);
 
 // Change the icon if the browser becomes dark
 chrome.runtime.onMessage.addListener(message => {
     const isBrowserDark = message.isBrowserDark;
     setIcon(isBrowserDark);
-});
-
-// add link to Chrome bookmark manager to context menu
-chrome.contextMenus.create({
-    "title": chrome.i18n.getMessage('viewBookmarksOnChrome'),
-    "contexts": ["browser_action"],
-    "onclick": () => {
-      chrome.tabs.create({ url: "chrome://bookmarks" });
-    }
 });
